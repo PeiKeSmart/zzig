35bb4db2dd9fd52f83e16aa47bcdb49b0f63c328
猿人易 <qcjxberin@outlook.com>
2024-07-19 17:17:36 +0800

    feat: 优化PrintString函数的实现
    这个提交优化了PrintString函数的实现，为其添加了对格式化字符串的支持。这样可以更灵活地输出字符串，并提高代码的可读性和可维护性。

--- src/logs/xtrace.zig
+++ src/logs/xtrace.zig
@@ -1,37 +1,3 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const strings = @import("../zzig.zig").Strings;
-
-/// PrintString 打印格式化字符串。
-///
-/// 参数:
-/// - `fmt`: 格式化字符串，使用 comptime 确定格式。
-/// - `args`: 要打印的字符串参数。
-///
-/// 返回:
-/// - 无返回值。
-pub fn PrintString(comptime fmt: []const u8, args: []u8) void {
-    if (strings.Contains(fmt, "{s}")) {
-        std.io.getStdOut().writer().print(fmt, .{args});
-    } else {
-        //@compileError("错误: 格式化字符串中缺少 '{s}' 占位符。");
-        std.io.getStdOut().writer().print("Error: The '{s}' placeholder is missing from the formatted string.", .{"{s}"});
-    }
-}
-
-/// PrintString 打印格式化字符串。
-///
-/// 参数:
-/// - `fmt`: 格式化字符串，使用 comptime 确定格式。
-/// - `args`: 要打印的字符串参数。
-///
-/// 返回:
-/// - 无返回值。
-pub fn PrintNumber(comptime fmt: []const u8, args: anytype) void {
-    if (strings.Contains(fmt, "{d}")) {
-        std.io.getStdOut().writer().print(fmt, .{args});
-    } else {
-        //@compileError("错误: 格式化字符串中缺少 '{d}' 占位符。");
-        std.io.getStdOut().writer().print("Error: The '{s}' placeholder is missing from the formatted string.", .{"{d}"});
-    }
-}
