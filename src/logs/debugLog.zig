487b3556d2a90c8189e88ee7ca2fc99a8f1055a7
猿人易 <qcjxberin@outlook.com>
2024-07-19 12:25:52 +0800

    feat: Update PrintString function to PrintStrings for printing multiple strings
    The PrintString function in the debugLog.zig file has been renamed to PrintStrings to better reflect its purpose of printing multiple strings. This change improves the clarity and consistency of the function name in the codebase.

--- src/logs/debugLog.zig
+++ src/logs/debugLog.zig
@@ -1,13 +1,24 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-/// PrintString 打印字符串。
+/// PrintStrings 打印字符串。
 ///
 /// 参数:
 /// - args: 用于拼接字符串的参数列表。
 ///
 /// 返回:
 /// - 无返回值。
-pub fn PrintString(args: []const []const u8) void {
+pub fn PrintStrings(args: []const []const u8) void {
+    std.debug.print("{s}\n", .{args});
+}
+
+/// PrintString 打印字符串。
+///
+/// 参数:
+/// - args: 字符串。
+///
+/// 返回:
+/// - 无返回值。
+pub fn PrintString(args: []u8) void {
     std.debug.print("{s}\n", .{args});
 }
