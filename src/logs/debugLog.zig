be6c037689bce9590b62cea890c676f8bed2f535
猿人易 <qcjxberin@outlook.com>
2024-07-19 15:54:26 +0800

    feat: Improve PrintString function by adding support for formatted strings
    The PrintString function in the debugLog.zig file now accepts a formatted string and a variable number of arguments, allowing for more flexible and customizable output. This enhancement improves the functionality and versatility of the PrintString function in the codebase.

--- src/logs/debugLog.zig
+++ src/logs/debugLog.zig
@@ -2,17 +2,6 @@ const std = @import("std");
 const builtin = @import("builtin");
 const strings = @import("../zzig.zig").Strings;
 
-/// PrintStrings 打印字符串。
-///
-/// 参数:
-/// - args: 用于拼接字符串的参数列表。
-///
-/// 返回:
-/// - 无返回值。
-pub fn PrintStrings(args: []const []const u8) void {
-    std.debug.print("{s}\n", .{args});
-}
-
 /// PrintString 打印格式化字符串。
 ///
 /// 参数:
@@ -27,5 +16,22 @@ pub fn PrintString(comptime fmt: []const u8, args: []u8) void {
     } else {
         //@compileError("错误: 格式化字符串中缺少 '{s}' 占位符。");
         std.debug.print("Error: The '{s}' placeholder is missing from the formatted string.", .{"{s}"});
+    }
+}
+
+/// PrintString 打印格式化字符串。
+///
+/// 参数:
+/// - `fmt`: 格式化字符串，使用 comptime 确定格式。
+/// - `args`: 要打印的字符串参数。
+///
+/// 返回:
+/// - 无返回值。
+pub fn PrintNumber(comptime fmt: []const u8, args: anytype) void {
+    if (strings.Contains(fmt, "{d}")) {
+        std.debug.print(fmt, .{args});
+    } else {
+        //@compileError("错误: 格式化字符串中缺少 '{d}' 占位符。");
+        std.debug.print("Error: The '{s}' placeholder is missing from the formatted string.", .{"{d}"});
     }
 }
